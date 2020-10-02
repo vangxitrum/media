9a50cbd5af6d3177c2dad24f66e5dfa18d578d31
vangxitrum <64908121+vangxitrum@users.noreply.github.com>
2020-10-02 16:09:38 +0700

    Add files via upload

---
+++ testMedia/testMedia/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testMedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Multiselect = true, ValidateNames = true, Filter = "WMV|*.wmv|WAV|*.wav|MP3|*.mp3|MP4|*.mp4"
+            })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    List<MediaFile> files = new List<MediaFile>();
+                    foreach (string fileName in ofd.FileNames)
+                    {
+                        FileInfo fi = new FileInfo(fileName);
+                        files.Add(new MediaFile() { fileName = Path.GetFileNameWithoutExtension(fi.FullName), path = fi.FullName });
+                    }
+                    listBox1.DataSource = files;
+                    
+
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MediaFile file = listBox1.SelectedItem as MediaFile;
+            if (file != null)
+            {
+                axWindowsMediaPlayer1.URL = file.path;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.ValueMember = "Path";
+            listBox1.DisplayMember = "Filename";
+        }
+    }
+}
